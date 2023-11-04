6d1c83206825f702af18e923109e15b9d1abecb5
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-11-04 12:04:44 -0300

    autohide objective panel

--- UI/HUD/HUD.gd
+++ UI/HUD/HUD.gd
@@ -1,5 +1,6 @@
 extends Control
 @export var objective_time := 3
+@export var objective_fade_time := 2
 
 func _ready():
 	show_objective()
@@ -8,7 +9,10 @@ func show_objective():
 	get_tree().create_timer(objective_time).timeout.connect(hide_objective)
 
 func hide_objective():
-	pass
+	create_tween()\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_EXPO)\
+		.tween_property($ObjectivePanel, "position:y", position.y + size.y, objective_fade_time)
 
 func update_timer(time_left : float, full_time : float):
 	%ObjectiveProgress.ratio = 1 - (time_left / full_time)
